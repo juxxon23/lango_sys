f14199cc205bdb574506af14f21ca8d8dd134601
juxxon23 <juxxon23@hotmail.com>
2025-09-11 22:10:31 -0500

    add useful scripts Delete db and insert default tags

---
+++ db/delete_db.sql
@@ -0,0 +1,1 @@
+DROP DATABASE lango_db;
---
+++ db/insert_tags.sql
@@ -0,0 +1,12 @@
+INSERT INTO tags (name, description) VALUES 
+('s', 'sustantivo'),
+('v', 'verbo'),
+('ad', 'adjetivo'),
+('av', 'adverbio'),
+('pp', 'preposicion'),
+('cn', 'conjuncion'),
+('c', 'correlativo'),
+('f', 'uso frecuente'),
+('xn', 'contexto natural'),
+('xt', 'contexto tecnologico'),
+('xm', 'contexto matematico');
